5084103cc4e32f6169ebc7c253d2bea789d34350
Fernando Carvalho <fernando_scarvalho@hotmail.com>
2026-01-14 16:14:54 -0300

    Config routes reset passowrd-3

--- src/server.ts
+++ src/server.ts
@@ -8,11 +8,24 @@ import authRoutes from './routes/auth.routes';
 import docsRoutes from './routes/docs.routes';
 
 const app = express();
-app.use(cors({
-  origin: "https://leandro-abreu-contabilidade.vercel.app", // SEM A BARRA "/" NO FINAL!
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Adicione OPTIONS
-  credentials: true
-}))
+app.use((req, res, next) => {
+  // 1. Quem pode acessar (Seu Frontend)
+  res.header("Access-Control-Allow-Origin", "https://leandro-abreu-contabilidade.vercel.app");
+  
+  // 2. Métodos permitidos
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
+  
+  // 3. Cabeçalhos permitidos
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With");
+  
+  // 4. A MÁGICA: Se for o "Preflight" (OPTIONS), responde 200 OK na hora e encerra!
+  if (req.method === 'OPTIONS') {
+      res.status(200).send();
+      return; 
+  }
+  
+  next(); // Se não for OPTIONS, segue para as rotas normais
+});
 
 app.use(express.json());
 app.use(authRoutes);
