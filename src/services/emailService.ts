ea4f854f0241f51a56c47a4cdf4330b1536adb03
Fernando Carvalho <fernando_scarvalho@hotmail.com>
2026-01-15 10:49:30 -0300

    Melhoria no email visual logo

--- src/services/emailService.ts
+++ src/services/emailService.ts
@@ -18,7 +18,7 @@ const REMETENTE_OFICIAL = `"Leandro Abreu Contabilidade" <${process.env.GOOGLE_E
 const gerarHtmlTemplate = (titulo: string, corpo: string, link: string, textoBotao: string) => {
   
   // ✅ Link direto da sua logo
-  const urlImagemRodape = "https://i.imgur.com/17XRFP8.png"; 
+  const urlImagemRodape = "https://drive.google.com/file/d/1ZB6szeWOo4oVGgWGheCQAw30vynuXK3y/view?usp=sharing"; 
 
   return `
     <!DOCTYPE html>
@@ -67,7 +67,7 @@ const gerarHtmlTemplate = (titulo: string, corpo: string, link: string, textoBot
               <tr>
                 <td bgcolor="#f9f9f9" style="padding: 30px 20px; text-align: center; border-top: 1px solid #eeeeee;">
                   
-                <blockquote class="imgur-embed-pub" lang="en" data-id="OjKLLSa"><a href="https://imgur.com/OjKLLSa">View post on imgur.com</a></blockquote><script async src="//s.imgur.com/min/embed.js" charset="utf-8"></script>
+                  <img src="${urlImagemRodape}" alt="Leandro Abreu Contabilidade" width="200" style="display: block; margin: 0 auto 20px auto; border: 0; max-width: 100%; height: auto;">
 
                   <p style="margin: 0; font-size: 12px; color: #999999; line-height: 1.5;">
                     &copy; ${new Date().getFullYear()} Leandro Abreu Contabilidade.<br>
